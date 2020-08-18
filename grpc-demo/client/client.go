1f140a281f45486b96653351e8c873289bcf0036
wujiajie <wujiajie1901@163.com>
2020-08-18 18:29:28 +0800

    添加grc-demo

---
+++ grpc-demo/client/client.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	pb "github.com/go-programming-tour-book/grpc-demo/proto"
+	"google.golang.org/grpc"
+	"log"
+)
+
+var (
+	port = "8000"
+)
+
+func main() {
+	conn, err := grpc.Dial(":"+port, grpc.WithInsecure())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	client := pb.NewGreeterClient(conn)
+	err = SayHello(client)
+	if err != nil {
+		log.Fatalf("SayHello err: %s", err.Error())
+	}
+}
+
+func SayHello(client pb.GreeterClient) error {
+	resp, err := client.SayHello(context.Background(), &pb.HelloRequest{
+		Name: "test",
+	})
+	if err != nil {
+		return err
+	}
+	log.Printf("client.SayHello resp: %s", resp.Message)
+	return nil
+}
---
+++ grpc-demo/server/server.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"flag"
+	pb "github.com/go-programming-tour-book/grpc-demo/proto"
+	"google.golang.org/grpc"
+	"net"
+	"strconv"
+)
+
+var (
+	port string
+)
+
+func init() {
+	flag.StringVar(&port, "p", "8000", "启动端口号")
+	flag.Parse()
+}
+
+type GreeterServer struct {
+}
+
+// 服务端流式RPC
+func (s *GreeterServer) SayList(req *pb.HelloRequest, stream pb.Greeter_SayListServer) error {
+	for n := 0; n < 6; n++ {
+		err := stream.Send(&pb.HelloReply{
+			Message: "hello.list" + strconv.Itoa(n),
+		})
+		if err != nil {
+			return nil
+		}
+	}
+	return nil
+}
+
+// 一元RPC
+func (s *GreeterServer) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloReply, error) {
+	return &pb.HelloReply{
+		Message: req.Name,
+	}, nil
+}
+
+func main() {
+	server := grpc.NewServer()
+	pb.RegisterGreeterServer(server, &GreeterServer{})
+	lis, _ := net.Listen("tcp", ":"+port)
+	server.Serve(lis)
+}
