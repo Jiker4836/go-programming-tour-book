1f04e5c0d111d80232d2f82652301a005ddd53a8
wujiajie <wujiajie@medlinker.com>
2020-11-03 00:23:05 +0800

    add config&db

--- blog-service/go.mod
+++ blog-service/go.mod
@@ -2,4 +2,8 @@ module github.com/go-programming-tour-book/blog-service
 
 go 1.13
 
-require github.com/gin-gonic/gin v1.6.3
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/jinzhu/gorm v1.9.12
+	github.com/spf13/viper v1.7.1
+)
--- blog-service/main.go
+++ blog-service/main.go
@@ -1,20 +1,33 @@
 package main
 
 import (
+	"github.com/go-programming-tour-book/blog-service/global"
+	"github.com/go-programming-tour-book/blog-service/internal/model"
 	"github.com/go-programming-tour-book/blog-service/internal/routers"
+	"github.com/go-programming-tour-book/blog-service/pkg/setting"
+	"log"
 	"net/http"
 	"time"
 )
 
+func init() {
+	if err := setupSetting(); err != nil {
+		log.Fatalf("init.setupSetting err: %v", err)
+	}
+	if err := setupDBEngine(); err != nil {
+		log.Fatalf("init.setupDBEngine err: %v", err)
+	}
+}
+
 func main() {
 	router := routers.NewRouter()
 	s := &http.Server{
-		Addr:              ":8080",
+		Addr:              ":" + global.ServerSetting.HttpPort,
 		Handler:           router,
 		TLSConfig:         nil,
-		ReadTimeout:       10 * time.Second,
+		ReadTimeout:       global.ServerSetting.ReadTimeout,
 		ReadHeaderTimeout: 0,
-		WriteTimeout:      10 * time.Second,
+		WriteTimeout:      global.ServerSetting.WriteTimeout,
 		IdleTimeout:       0,
 		MaxHeaderBytes:    1 << 20,
 		TLSNextProto:      nil,
@@ -24,4 +37,35 @@ func main() {
 		ConnContext:       nil,
 	}
 	s.ListenAndServe()
+}
+
+func setupSetting() error {
+	setting, err := setting.NewSetting()
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("Server", &global.ServerSetting)
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("App", &global.AppSetting)
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("Database", &global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+	global.ServerSetting.ReadTimeout *= time.Second
+	global.ServerSetting.WriteTimeout *= time.Second
+	return nil
+}
+
+func setupDBEngine() error {
+	var err error
+	global.DBEngine, err = model.NewDBEngine(global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+	return nil
 }
